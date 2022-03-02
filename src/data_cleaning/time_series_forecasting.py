e0597c7248a9fbecd6f49336ff7c146bb7257185
unknown <louisdumontet@gmail.com>
2022-03-02 17:37:06 +0100

    Time serie dataset creation

---
+++ src/data_cleaning/time_series_forecasting.py
@@ -0,0 +1,41 @@
+import pandas as pd 
+import numpy as np
+import tensorflow as tf
+from window_generator import WindowGenerator
+from os import listdir
+from os.path import isfile, join
+
+def create_dataset(window_size=6, train_size=0.8, val_size=0.1, test_size=0.1) :
+
+    assert(train_size + val_size + test_size == 1)
+    assert(window_size >= 2)
+
+    path = '../../data/formated_csv/'
+    filanemes = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
+
+    for i, file in enumerate(filanemes) :
+
+        df = pd.read_csv(path + file)
+
+        df.set_index('Temps', inplace=True)
+        df.drop(columns=['Vitesse de vent'], axis=0, inplace=True)
+        df.dropna(inplace=True)
+
+        n = len(df)
+        train_df = df.iloc[0:int(n*train_size)]
+        val_df = df.iloc[int(n*train_size):int(n*(train_size+val_size))]
+        test_df = df.iloc[int(n*(train_size+val_size)):]
+
+        new = WindowGenerator(window_size, 1, 1, train_df, val_df, test_df, ['Altitude', 'Latitude', 'Longitude'])
+
+        if i != 0 :
+            train = train.concatenate(new.train)
+            val = val.concatenate(new.val)
+            test = test.concatenate(new.test)
+        
+        else :
+            train = new.train
+            val = new.val
+            test = new.test
+    
+    return train, val, test
